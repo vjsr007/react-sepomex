f095a7800014233eb9e90abffaaa3b9f8b026c8c
vjsr007 <vjsr007...>
2017-01-13 12:32:56 -0600

    Layout Inicial ReactJS MVC .Net

---
+++ reactjsnet/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Script.Serialization;
+using System.Globalization;
+using System.Threading;
+
+namespace reactjsnet
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ReactConfig.Configure();
+            
+        }
+    }
+}
